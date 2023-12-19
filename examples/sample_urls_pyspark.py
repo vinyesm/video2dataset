baf5157b90f18a2fc2f37dbe61e4c28e40fe9b40
Marina Vinyes <vinyes.marina@gmail.com>
2023-12-19 22:46:08 +0100

    script to count domains using pyspark

---
+++ examples/sample_urls_pyspark.py
@@ -0,0 +1,26 @@
+
+from pyspark.sql.functions import expr, col
+from pyspark.sql import SparkSession
+import fire
+
+
+def domain_count(url_list: str, output_folder: str):
+    spark = SparkSession.builder.appName("DomainCount").getOrCreate()
+    spark.sql("set spark.sql.files.ignoreCorruptFiles=true")
+    df = spark.read.parquet(url_list)
+    result_df = df.withColumn("domain", expr("parse_url(url, 'HOST')")) \
+        .withColumn("domain", expr("lower(coalesce(substring_index(domain, 'www.', -1), domain))")) \
+        .groupBy("domain").count().orderBy(col("count").desc())
+    result_df.repartition(1).write.parquet(output_folder, mode="overwrite", compression="snappy")
+    return
+
+
+          
+if __name__ == "__main__":
+    """
+    Usage: sample_urls_large_pyspark --url_list=[url_list] --output_folder=[output_folder]
+    Example: sample_urls_large_pyspark --url_list="s3a://my-content/video/one_million.parquet" --output_folder="s3a://my-content/video/urls/video_platform_dataset-stats"
+
+    Counts the number of videos per domain in a dataset of urls and save the result to a parquet file
+    """
+    fire.Fire(domain_count)
