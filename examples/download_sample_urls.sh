50e2f707e20904038f848240b600e6bf553d9d3f
Ubuntu <ubuntu@ip-172-31-31-150.ec2.internal>
2024-01-03 14:32:36 +0000

    minor param value change

--- examples/download_sample_urls.sh
+++ examples/download_sample_urls.sh
@@ -3,5 +3,5 @@
 # https://wandb.ai/authorize
 WANDB_API_KEY=""
 
-video2dataset --url_list="s3://my-content/video/urls/video_platform_dataset-sample/one_million/" --url_col="url" --output_folder="s3://my-content/video/urls/video_platform_dataset-sample/one_million_dataset_v15" --input_format="parquet" --enable_wandb="False" --wandb_project="video-download"  --tmp_dir="./tmp"
+video2dataset --url_list="s3://my-content/video/urls/video_platform_dataset-sample/one_million/" --url_col="url" --output_folder="s3://my-content/video/urls/video_platform_dataset-sample/one_million_dataset_v16" --input_format="parquet" --enable_wandb="False" --wandb_project="video-download"  --tmp_dir="./tmp"
 
